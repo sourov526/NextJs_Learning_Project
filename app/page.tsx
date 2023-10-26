dc02efd1954a3a3f553fda447ed31876529269e2
sourov <sourovsh533@gmail.com>
2023-10-26 14:10:08 +0600

    basic routing and navigating

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <h1>Hello Bangladesh</h1>
       <h3>Hello Pakistan</h3>
       <h4>Hello India</h4>
+      <Link href="/users">Users</Link>
     </main>
   )
 }
